97317916a4d215ff063000d809e9122656edff8b
Dan Sanderson <contact@dansanderson.com>
2022-08-19 19:08:46 -0700

    Repair dist build by ordering SOURCES :(

--- tests/test_executor.c
+++ tests/test_executor.c
@@ -6,6 +6,9 @@ void setUp(void) {}
 void tearDown(void) {}
 
 void test_DoMath_WithPositive_Works(void) {
+    // (Intentionally mock the wrong answer to demonstrate that the mock is
+    // being called:)
+    cfgfile_add_one_ExpectAndReturn(7, 1000);
     int result = executor_do_math(7);
-    TEST_ASSERT_EQUAL_MESSAGE(56, result, "Does math");
+    TEST_ASSERT_EQUAL_MESSAGE(7000, result, "Does math");
 }
